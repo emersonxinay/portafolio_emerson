6e2651821f7f147478ee373d5fcc010263ae9cd0
emerson espinoza aguirre <emerson@emersons-MacBook-Air.local>
2021-10-06 09:21:08 -0500

    production

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Portafolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -19,5 +19,7 @@ module Portafolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.initialize_on_precompile = false
+    
   end
 end
